9e5e27715b99111e02e0dfad0bc1a6f0287b2a23
Stelios Mpetas <mpetas@exeo.gr>
2022-07-15 12:16:08 +0300

    v 0.0

--- ESATouristGuide/ESATouristGuide.Android/Properties/AssemblyInfo.cs
+++ ESATouristGuide/ESATouristGuide.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XFTemplateApp.Android")]
+[assembly: AssemblyTitle("ESATouristGuide.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XFTemplateApp.Android")]
+[assembly: AssemblyProduct("ESATouristGuide.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ESATouristGuide/ESATouristGuide.iOS/Properties/AssemblyInfo.cs
+++ ESATouristGuide/ESATouristGuide.iOS/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XFTemplateApp.iOS")]
+[assembly: AssemblyTitle("ESATouristGuide.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XFTemplateApp.iOS")]
+[assembly: AssemblyProduct("ESATouristGuide.iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
